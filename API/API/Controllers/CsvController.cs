8d38f489606221663bfa936caff8d9571375f7d5
Huda Syed <who.the.syed@gmail.com>
2018-08-12 10:27:02 -0700

    Upgraded .net core dependencies. Scaffolded Csv controller.

---
+++ API/API/Controllers/CsvController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/Csv")]
+    public class CsvController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> ParseCsv()
+        {
+            await Task.Delay(10);
+            return Ok();
+        }
+    }
+}
