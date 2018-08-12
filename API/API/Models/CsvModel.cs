7bbcecda328202864d7e633d65608d9c6378e860
Huda Syed <who.the.syed@gmail.com>
2018-08-12 10:41:17 -0700

    Added Newtonsoft JSON package and created model.

---
+++ API/API/Models/CsvModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class CsvModel
+    {
+        [JsonIgnore]
+        public int Id { get; set;  }
+        public string Parent { get; set; }
+        public string Child { get; set; }
+        public int Quantity { get; set; }
+    }
+}
